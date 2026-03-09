396e6502db6ad717379b02c8d2cf9005f9382924
Jeferson Alves <jefersonalvessantos62@gmail.com>
2026-03-08 22:57:30 -0300

    init: Aula de Array.

---
+++ array.js
@@ -0,0 +1,28 @@
+/*                  0         1         2          3          4
+// const arr = [elemento , elemento, elemento , elemento, elemento] */
+
+const arrNUmeros = [1, 2, 3];
+
+const arrMisto = [5, true, "texto"];
+
+const arrArrays = [
+  [1, 2],
+  ["a", "b", "c"],
+  [true, false],
+];
+
+const arrObjetos = [{ a: 1 }, { b: 2 }, { c: 3 }];
+
+// MANIPULANDO ARRAY
+
+const arrNumeros2 = [46, 34, 23, 12];
+
+console.log(arrNumeros2[0]);
+console.log(arrNumeros2[1]);
+console.log(arrNumeros2[2]);
+console.log(arrNumeros2[3]);
+
+arrNumeros2[1] = 36;
+arrNumeros2[4] = 100;
+
+console.log(arrNumeros2);
