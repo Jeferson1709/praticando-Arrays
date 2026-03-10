abb0e6d1f4cda27125df7de8b74e9138a3420b3b
Jeferson Alves <jefersonalvessantos62@gmail.com>
2026-03-09 23:36:50 -0300

    feat: Exercícios de Arrays.

---
+++ exercicios/exercicio5.js
@@ -0,0 +1,16 @@
+/* Você está desenvolvendo um sistema de tarefas do dia a dia. O sistema deve permitir que a pessoa usuária adicione uma nova tarefa ao final da lista e, em seguida, remova a última tarefa que foi adicionada. Escreva um programa que:
+
+Crie um array com três tarefas iniciais.
+Adicione uma nova tarefa no final da lista.
+Remova a última tarefa da lista.
+Exiba a lista após cada operação. */
+
+const tarefas = ["Estudar", "Lavar roupa", "Fazer compras"];
+
+tarefas.push("Limpar o quarto");
+
+tarefas.pop();
+console.log("lista após adicionar :", {tarefas});
+
+
+console.log("Lista após remover a última tarefa:",{tarefas});
